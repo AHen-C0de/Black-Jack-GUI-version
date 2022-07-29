18e5d70cc2ac4d5a1644bd654003109570397431
FlurryC0de <100531381+FlurryC0de@users.noreply.github.com>
2022-07-29 17:08:36 +0200

    Add files via upload

--- bj_constants.py
+++ bj_constants.py
@@ -11,14 +11,17 @@ window_size = (1200,600)
 
 FPS = 30
 
-script_path = Path(os.path.dirname(os.path.abspath(__file__)))
-cards_path  = Path(script_path, "images\cards")
+script_path  = Path(os.path.dirname(os.path.abspath(__file__)))
+images_path  = Path(script_path, "images")
 
-font = 'Corbel'
+textFont = 'Corbel'
 
 # Colors
-black = (0,0,0)
-window_color = (40,185,84)
-buttonArea_color = (233,244,122)
-color_active = (170,170,170)
-color_inactive = (100,100,100)
+black                 = (0,0,0)
+window_color          = (40,185,84)
+buttonArea_color      = (233,244,122)
+color_active          = (170,170,170)
+color_inactive        = (120,120,120)
+color_disabled        = (85,85,85)
+color_resultsBox      = (255,255,204)
+color_resultsBoxFont  = (204,204,0)
