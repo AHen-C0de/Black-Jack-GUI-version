2a59b72f6ab2497f12f437cc8a76faf6a5205f54
FlurryC0de <100531381+FlurryC0de@users.noreply.github.com>
2022-07-29 17:15:59 +0200

    Add files via upload
    final version

--- bj_deck_hand.py
+++ bj_deck_hand.py
@@ -8,19 +8,19 @@ import itertools as itt
 
 
 class Type(Enum):
-    Ace = 1
-    Two = 2
+    Ace   = 1
+    Two   = 2
     Three = 3
-    Four = 4
-    Five = 5
-    Six = 6
+    Four  = 4
+    Five  = 5
+    Six   = 6
     Seven = 7
     Eight = 8
-    Nine = 9
-    Ten = 10
-    Jack = 11
+    Nine  = 9
+    Ten   = 10
+    Jack  = 11
     Queen = 12
-    King = 13
+    King  = 13
     
 class Color(Enum):
     D = 1
@@ -36,13 +36,12 @@ class Card(NamedTuple):
 class Deck:
     def __init__(self):
         self.cards = []
-        self.build()
-        self.shuffle()
-        
+
     def build(self):
+        self.cards =  []
         for type in Type:
             for color in Color:
-                self.cards.append(Card(type,color))
+                self.cards.append(Card(type,color))            
                 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -58,9 +57,11 @@ class Hand():
         self.value = 0
         
     def add_card(self, card):
-        self.cards.append(card)
+        if card != None:
+            	self.cards.append(card)
         
     def calc_hand(self):
+        self.value = 0
         for card in self.cards:
             if card.type.name in {'Jack','Queen','King'}:
                 self.value += 10
